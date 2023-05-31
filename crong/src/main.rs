f55aa8e63eeb908fc5eb7ffaa179b573d636f02d
Alan <alan@alanklas.com>
2023-06-01 00:46:48 +0200

    threaded ui example in crong rn

--- crong/src/main.rs
+++ crong/src/main.rs
@@ -1,81 +1,46 @@
 use macroquad::prelude::*;
+use std::sync::{Arc, Mutex};
+use std::thread;
 
-fn window_conf() -> Conf {
-    Conf {
-        window_title: "Button Example".to_owned(),
-        ..Default::default()
-    }
-}
-
-#[macroquad::main(window_conf)]
+#[macroquad::main("Threaded UI Example")]
 async fn main() {
-    let mut button = Button::new(100.0, 100.0, 200.0, 50.0);
+    // Shared counter value protected by a Mutex
+    let counter = Arc::new(Mutex::new(0));
+
+    // Spawn a separate thread to increment the counter
+    let counter_thread = {
+        let counter = Arc::clone(&counter);
+        thread::spawn(move || {
+            loop {
+                // Increment the counter
+                {
+                    let mut counter = counter.lock().unwrap();
+                    *counter += 1;
+                }
+            }
+        })
+    };
 
     loop {
-        clear_background(Color::from_rgba(0, 0, 0, 255));
+        clear_background(BLACK);
 
-        if button.draw() {
-            println!("Hello, World!");
-        }
-
-        next_frame().await
-    }
-}
-
-struct Button {
-    x: f32,
-    y: f32,
-    width: f32,
-    height: f32,
-    clicked: bool,
-}
-
-impl Button {
-    fn new(x: f32, y: f32, width: f32, height: f32) -> Button {
-        Button {
-            x,
-            y,
-            width,
-            height,
-            clicked: false,
-        }
-    }
-
-    fn draw(&mut self) -> bool {
-        let mouse_position = mouse_position();
-
-        let is_hovered = self.is_point_inside(mouse_position.0, mouse_position.1);
-
-        let is_pressed = is_mouse_button_down(MouseButton::Left);
-
-        let is_released = !is_pressed && self.clicked;
-
-        if is_hovered && is_released {
-            self.clicked = false;
-            return true;
-        }
-
-        self.clicked = is_pressed && is_hovered;
-
-        draw_rectangle(self.x, self.y, self.width, self.height, WHITE);
-
-        let text = if self.clicked {
-            "Click me!"
-        } else {
-            "Hello, World!"
+        // Display the counter value
+        let counter_value = {
+            let counter = counter.lock().unwrap();
+            *counter
         };
 
-        let text_width = measure_text(text, None, 20, 1.0).width;
+        draw_text(
+            &format!("Counter: {}", counter_value),
+            10.0,
+            30.0,
+            30.0,
+            WHITE,
+        );
 
-        let text_x = self.x + (self.width - text_width) / 2.0;
-        let text_y = self.y + self.height / 2.0 - 10.0;
-
-        draw_text(text, text_x, text_y, 20.0, BLACK);
-
-        false
+        next_frame().await;
     }
 
-    fn is_point_inside(&self, x: f32, y: f32) -> bool {
-        x >= self.x && x <= self.x + self.width && y >= self.y && y <= self.y + self.height
-    }
+    // Join the counter thread to ensure it finishes
+    counter_thread.join().unwrap();
 }
