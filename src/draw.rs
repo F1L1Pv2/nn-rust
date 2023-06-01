F1L1Pv2/nn-rust

commit a2b2ee68e4134bfcf964d090697171be8b2a554b

fixed dynamic input

src/draw.rs
@@ -3,6 +3,20 @@ use super::{
     Color, Renderinfo, EPOCH_MAX, GRAY, LEARNING_RATE, LINE_COLOR, NN, TEXT_COLOR,
 };
 
+const LOW_COLOR: Color = Color {
+    r: 0.,
+    g: 1.,
+    b: 0.,
+    a: 1.,
+};
+
+const HIGH_COLOR: Color = Color {
+    r: 1.,
+    g: 0.,
+    b: 1.,
+    a: 1.,
+};
+
 pub fn draw_frame(nn: &NN, width: f32, height: f32, info: &Renderinfo) {
     let nn = nn.clone();
 
@@ -18,19 +32,6 @@ pub fn draw_frame(nn: &NN, width: f32, height: f32, info: &Renderinfo) {
 }
 
 fn draw_nn(nn: &NN, width: f32, height: f32) {
-    let low_color = Color {
-        r: 1.,
-        g: 0.,
-        b: 1.,
-        a: 1.,
-    };
-    let high_color = Color {
-        r: 0.,
-        g: 1.,
-        b: 0.,
-        a: 1.,
-    };
-
     let x = 0.;
     let y = 0.;
 
@@ -62,7 +63,7 @@ fn draw_nn(nn: &NN, width: f32, height: f32) {
                         cx2,
                         cy2,
                         thick,
-                        color_lerp(low_color, high_color, value),
+                        color_lerp(LOW_COLOR, HIGH_COLOR, value),
                     );
                 }
             }
@@ -72,7 +73,7 @@ fn draw_nn(nn: &NN, width: f32, height: f32) {
                     cx1,
                     cy1,
                     neuron_radius,
-                    color_lerp(low_color, high_color, value),
+                    color_lerp(LOW_COLOR, HIGH_COLOR, value),
                 );
             } else {
                 draw_circle(cx1, cy1, neuron_radius, GRAY);
@@ -170,8 +171,9 @@ fn draw_data(info: &Renderinfo, mut nn: NN) {
 
     // Write the testing results at the bottom left
     for i in 0..info.t_input.rows {
-        nn.activations[0].data[0][0] = info.t_input.data[i][0];
-        nn.activations[0].data[0][1] = info.t_input.data[i][1];
+        for j in 0..nn.activations[0].rows {
+            nn.activations[0].data[j][0] = info.t_input.data[i][j];
+        }
 
         NN::forward(&mut nn);
         draw_text(

src/main.rs
@@ -47,12 +47,43 @@ impl Renderinfo {
 #[macroquad::main(window_conf)]
 async fn main() {
     'main: loop {
-        let nn = Arc::new(Mutex::new(NN::new(&[2, 4, 1])));
-        let mut g = NN::new(&[2, 4, 1]);
+        let nn_structure = &[1, 5,  1];
+
+        let nn = Arc::new(Mutex::new(NN::new(nn_structure)));
+        let mut g = NN::new(nn_structure);
 
         // XOR Example
-        let t_input = Mat::new(&[&[0.0, 0.0], &[0.0, 1.0], &[1.0, 0.0], &[1.0, 1.0]]);
-        let t_output = Mat::new(&[&[0.0], &[1.0], &[1.0], &[0.0]]);
+        // let t_input = Mat::new(&[&[0.0, 0.0], &[0.0, 1.0], &[1.0, 0.0], &[1.0, 1.0]]);
+        // let t_output = Mat::new(&[&[0.0], &[1.0], &[1.0], &[0.0]]);
+
+        // Opposite example
+        let t_input = Mat::new(&[
+            &[1.0],
+            &[0.9],
+            &[0.8],
+            &[0.7],
+            &[0.6],
+            &[0.5],
+            &[0.4],
+            &[0.3],
+            &[0.2],
+            &[0.1],
+            &[0.0],
+        ]);
+
+        let t_output = Mat::new(&[
+            &[0.0],
+            &[0.1],
+            &[0.2],
+            &[0.3],
+            &[0.4],
+            &[0.5],
+            &[0.6],
+            &[0.7],
+            &[0.8],
+            &[0.9],
+            &[1.0],
+        ]);
 
         {
             let mut nn = nn.lock().unwrap();
@@ -124,8 +155,9 @@ async fn main() {
                 let output;
                 {
                     let mut nn = nn_clone.lock().unwrap();
-                    nn.activations[0].data[0][0] = t_input.data[i][0];
-                    nn.activations[0].data[0][1] = t_input.data[i][1];
+                    for j in 0..nn.activations[0].rows {
+                        nn.activations[0].data[j][0] = t_input.data[i][j];
+                    }
 
                     NN::forward(&mut nn);
                     output = nn.activations[nn.count - 1].data[0].clone();

commit 1cbe3da2e79ba4deb378ddf8375018d7e45fe543

fixed the stupid output error thing after a few hours of debugging, commit a2b2ee6 broke it

src/draw.rs
@@ -181,8 +181,8 @@ fn draw_data(info: &Renderinfo, mut nn: NN) {
 
     // Write the testing results at the bottom left
     for i in 0..info.t_input.rows {
-        for j in 0..nn.activations[0].rows {
-            nn.activations[0].data[j][0] = info.t_input.data[i][j];
+        for j in 0..nn.activations[0].data[0].len() {
+            nn.activations[0].data[0][j] = info.t_input.data[i][j];
         }
 
         NN::forward(&mut nn);
@@ -191,7 +191,7 @@ fn draw_data(info: &Renderinfo, mut nn: NN) {
                 // Input | Output
                 "{:?} -> {:?}",
                 info.t_input.data[i],
-                nn.activations[nn.count - 1].data[0]
+                nn.activations[nn.count - 1].data[0] // -1 because the last activation is the output
             )
             .as_str(),
             0.,

src/main.rs
@@ -31,7 +31,7 @@ enum Signal {
 
 #[macroquad::main(window_conf)]
 async fn main() {
-    let nn_structure = &[2, 4, 1];
+    let nn_structure = &[2, 4, 4, 1];
     let nn = Arc::new(Mutex::new(NN::new(nn_structure)));
     let gradient = NN::new(nn_structure);
 